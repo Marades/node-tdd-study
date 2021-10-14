f827aa40ff9291f2ab40c9cdc5615320da9068ad
김건회 <jkl9510357@gmail.com>
2021-10-14 21:42:06 +0900

    Add: mongodb 연결 및 스키마 설정

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ const PORT = 8080;
 const HOST = "0.0.0.0";
 
 const app = express();
-app.use(express.json());
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://marades:4202@node-tdd-study.zcvzs.mongodb.net/node-tdd-study?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("mongo connect"))
+  .catch((e) => console.log(e));
 
 const productRoutes = require("./routes");
 
+app.use(express.json());
 app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
